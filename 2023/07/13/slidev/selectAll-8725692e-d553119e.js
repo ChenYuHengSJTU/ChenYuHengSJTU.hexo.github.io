3e9c37cdc80b25fc390bf53eb2da7e59e24dce6d
ChenYuHeng <107762745+ChenYuHengSJTU@users.noreply.github.com>
2023-07-14 10:06:26 +0800

    Site updated: 2023-07-14 10:06:21

---
+++ 2023/07/13/slidev/selectAll-8725692e-d553119e.js
@@ -0,0 +1,1 @@
+import{bg as t,bh as n,bi as o}from"./index-6b082c44.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
